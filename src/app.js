0b04ed70fbd7bf6010879f0feea500da4c221523
wrxzzox <porkuang2548@gmail.com>
2025-04-24 22:45:34 +0700

    update app.js and app.test.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ const dayOfTheWeek = (date = new Date()) => {
     return days[date.getDay()];
 }
 
+const monthOfTheYear = (date = new Date()) => {
+    const month = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
+    return month[date.getMonth()];
+}
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElementById('month').innerText = monthOfTheYear();
 } catch(err) {}
 
-
 exports.dayOfTheWeek = dayOfTheWeek;
+exports.monthOfTheYear = monthOfTheYear;
